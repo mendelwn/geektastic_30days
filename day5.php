d9e610a239cc4f29c5533d9653fdd04a350b0431
Mendel <mendel.winiawer@multidadosti.com.br>
2022-10-05 18:26:15 -0300

    Day 5

---
+++ day5.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * Given an integer, $n, print its first 10 multiples.
+ * Each multiple (where 1 < i < 10) should be printed on a new line in the form: n x i = result.
+
+Example $n = 3
+The printout should look like this:
+
+3 x 1 = 3
+3 x 2 = 6
+3 x 3 = 9
+3 x 4 = 12
+3 x 5 = 15
+3 x 6 = 18
+3 x 7 = 21
+3 x 8 = 24
+3 x 9 = 27
+3 x 10 = 30
+
+ * */
+
+$n = intval(trim(fgets(STDIN)));
+
+for($i = 1; $i <= 10; $i++)
+    print("$n x $i = " . ($n*$i) . "\n");
